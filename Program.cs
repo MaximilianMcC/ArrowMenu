c4400ba2ade0a67f448ea1d577fb1f63ff329958
Max <max@maximilian.co.nz>
2023-02-07 08:02:46 +1300

    Restarting everything

--- Program.cs
+++ Program.cs
@@ -1,18 +1,3 @@
-﻿ArrowMenu menu = new ArrowMenu()
-{
-    title = "Which of the above planets is largest?",
-    menuItems = new[] { "Mercury", "Earth", "Jupiter", "Neptune" },
-    decorationCharacters = 4,
-    padding = 5
-};
-
-
-int answer = menu.VerticalMenu();
-
-// Check for if they selected the correct answer
-if (answer == 2) Console.WriteLine("Correct!");
-else Console.WriteLine("Incorrect!");
-
-
+﻿
 // Stop the program from closing
 Console.ReadKey();
