41d5e3380671a3d0d9f74a2771e7e04e2ba901e9
MaximilianMcC <max@maximilian.co.nz>
2023-05-17 10:24:28 +1000

    Created the arrow menu.

--- ArrowMenu.cs
+++ ArrowMenu.cs
@@ -1,117 +1,79 @@
-class MenuStyle
-{
-	/// <summary>
-	/// Characters that will be placed before the current menu item.
-	/// </summary>
-	public string prefix { get; set; }
-
-	/// <summary>
-	/// The line style of the menu.
-	/// </summary>
-	public LineStyle lineStyle;
-
-	/// <summary>
-	/// Should the menu spawn the entire width of the console.
-	/// </summary>
-	public bool fullWidth;
-}
-
-enum LineStyle
-{
-	/// <summary>
-	/// Thick lines such as "═══"
-	/// </summary>
-	THICK,
-
-	/// <summary>
-	/// Thin lines such as "───"
-	/// </summary>
-	THIN
-}
-
-
 class ArrowMenu
 {
-	string[] menuItems;
-	MenuStyle menuStyle;
-
-
-	// Constructor that makes a new arrow menu
-	public ArrowMenu(MenuStyle menuStyle, string[] menuItems, string title)
+	private Line line;
+	private int width;
+	public ArrowMenu(LineStyle lineStyle = LineStyle.THIN)
 	{
-		this.menuItems = menuItems;
-		this.menuStyle = menuStyle;
+		// Set the line style
+		line = new Line(lineStyle);
 	}
 
 
-	public int Menu()
+	public int Menu(string[] menuItems)
 	{
 		int index = 0;
-		int menuTop = Console.CursorTop + 1;
-		int menuWidth = 0;
+		int padding = 5;
+		int top = Console.CursorTop + 1;
 
+		// Hide the cursor while the menu is active
+		Console.CursorVisible = false;
 
-		// Draw the menu borders
-		if (menuStyle.fullWidth == true)
+		// Get the longest item in the menu
+		int longest = 0;
+		for (int i = 0; i < menuItems.Length; i++)
 		{
-			DrawBox(Console.WindowWidth - 1, menuItems.Length);
-
-			// Assign the inner menu width
-			menuWidth = Console.WindowWidth - 3;
+			if (menuItems[i].Length > longest) longest = menuItems[i].Length;
 		}
-		else
+		width = (longest + (padding * 2));
+
+		// Add the initial menu items
+		Console.WriteLine(line.topLeft + new string(line.horizontal, width) + line.topRight);
+		Console.CursorTop = top + menuItems.Length;
+		Console.WriteLine(line.bottomLeft + new string(line.horizontal, width) + line.bottomRight);
+		Console.CursorTop = top;
+		for (int i = 0; i < menuItems.Length; i++)
 		{
-			// Calculate the total width of the menu
-			int menuBoxWidth = menuItems.Max(menuItem => menuItem.Length);
-
-			// Apply padding and prefix
-			menuBoxWidth += 10;
-			menuBoxWidth += menuStyle.prefix.Length;
-
-			// Draw the menu borders
-			DrawBox(menuBoxWidth, menuItems.Length);
-
-			// Assign the inner menu width
-			menuWidth = menuBoxWidth - 2;
+			Console.SetCursorPosition(0, (top + i));
+			DrawItem(menuItems[i], i == index);
 		}
-		
 
-		// Add in all of the menu items
+		// Menu loop
 		while (true)
 		{
-
-			// Loop over all menu items
+			// Highlight the selected index
 			for (int i = 0; i < menuItems.Length; i++)
 			{
-				// Set the correct cursor position
-				Console.SetCursorPosition(1, menuTop + i);
-
-				// Check for if the current one is selected, else draw it normally
-				if (index == i)
-				{
-					// Switch the colors
-					Console.ForegroundColor = ConsoleColor.Black;
-					Console.BackgroundColor = ConsoleColor.White;
-
-					// Add the background color thing
-					Console.Write(new string(' ', menuWidth));
-
-					// Add the menu item
-					Console.SetCursorPosition(1, Console.CursorTop);
-					Console.Write(menuStyle.prefix + menuItems[i]);
-				}
-				else Console.Write(new string(' ', menuStyle.prefix.Length) + menuItems[i]);
-
-
-				// Set the colors back to the original
-				Console.ResetColor();
+				// Draw the current menu item
+				Console.SetCursorPosition(0, top + i);
+				DrawItem(menuItems[i], i == index);
 			}
 
-			break;
+			// Get the user input
+			ConsoleKeyInfo input = Console.ReadKey(true);
+			if (input.Key == ConsoleKey.UpArrow)
+			{
+				// Go up
+				index--;
+			}
+			else if (input.Key == ConsoleKey.DownArrow || input.Key == ConsoleKey.Tab)
+			{
+				// Go down
+				index++;
+			}
+			else if (input.Key == ConsoleKey.Enter)
+			{
+				// Reset the cursor
+				Console.SetCursorPosition(0, (top + menuItems.Length + 1));
+				Console.CursorVisible = true;
+
+				// Submit the current index as the answer
+				return index;
+			}
+
+			// Check for if the new index is out of bounds
+			if (index == -1) index = (menuItems.Length - 1);
+			if (index == menuItems.Length) index = 0;
 		}
-
-
-		return 0;
 	}
 
 
@@ -119,35 +81,58 @@ class ArrowMenu
 
 
 
-
-
-
-
-	void DrawBox(int width, int height)
+	private void DrawItem(string item, bool selected)
 	{
-		// Trim the width to take into account the sides
-		width -= 2;
+		// Draw the left border
+		Console.Write(line.vertical);
 
-		// Get the line style
-		//TODO: Don't use empty character
-		char topLeft = '\0';
-		char topRight = '\0';
-		char bottomLeft = '\0';
-		char bottomRight = '\0';
-		char horizontal = '\0';
-		char vertical = '\0';
-
-		// Assign the line style
-		if (menuStyle.lineStyle == LineStyle.THICK)
+		// Check for if the current item is selected
+		if (selected == true)
 		{
-			topLeft = '╔';
-			topRight = '╗';
-			bottomLeft = '╚';
-			bottomRight = '╝';
-			horizontal = '═';
-			vertical = '║';
+			// Flip the console colors
+			ConsoleColor background = Console.BackgroundColor;
+			Console.BackgroundColor = Console.ForegroundColor;
+			Console.ForegroundColor = background;
+
+			// Add a arrow thing
+			Console.Write("> ");
 		}
-		else if (menuStyle.lineStyle == LineStyle.THIN)
+		else Console.Write("  ");
+
+		// Add the text and whitespace, then revert the colors
+		Console.Write(item);
+		Console.Write(new string(' ', width - item.Length - 2));
+		Console.ResetColor();
+
+		// Draw the the right border
+		Console.Write(line.vertical);
+	}
+
+}
+
+
+
+
+
+// Line stuff
+public enum LineStyle
+{
+	THIN,
+	THICK,
+	CLASSIC
+}
+class Line
+{
+	public char topLeft { get; }
+	public char topRight { get; }
+	public char bottomLeft { get; }
+	public char bottomRight { get; }
+	public char horizontal { get; }
+	public char vertical { get; }
+
+	public Line(LineStyle lineStyle)
+	{
+		if (lineStyle == LineStyle.THIN)
 		{
 			topLeft = '┌';
 			topRight = '┐';
@@ -156,19 +141,23 @@ class ArrowMenu
 			horizontal = '─';
 			vertical = '│';
 		}
-
-
-		// Draw the top of the box
-		Console.WriteLine(topLeft + new string(horizontal, width) + topRight);
-		
-		// Draw the middle/walls of the box
-		for (int i = 0; i < height; i++)
+		else if (lineStyle == LineStyle.THICK)
 		{
-			Console.WriteLine(vertical + new string(' ', width) + vertical);
+			topLeft = '╔';
+			topRight = '╗';
+			bottomLeft = '╚';
+			bottomRight = '╝';
+			horizontal = '═';
+			vertical = '║';
 		}
-
-		// Draw the bottom of the box
-		Console.WriteLine(bottomLeft + new string(horizontal, width) + bottomRight);
-
+		else if (lineStyle == LineStyle.CLASSIC)
+		{
+			topLeft = '+';
+			topRight = '+';
+			bottomLeft = '+';
+			bottomRight = '+';
+			horizontal = '-';
+			vertical = '|';
+		}
 	}
 }
