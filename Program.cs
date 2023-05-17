406a585ac9b22aa66516dca412af72e7baca38df
MaximilianMcC <max@maximilian.co.nz>
2023-05-17 10:24:13 +1000

    Added an example of using it

--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
-﻿// Make a menu style
-MenuStyle menuStyle = new MenuStyle();
-menuStyle.prefix = "->";
-menuStyle.lineStyle = LineStyle.THICK;
-
-// Make an arrow menu
-string[] menuItems = new string[]
+﻿class Program
 {
-    "Option 1",
-    "Option 2",
-    "Option 3",
-    "Option 4",
-    "Option 5"
-};
-ArrowMenu arrowMenu = new ArrowMenu(menuStyle, new[] {"e"}, "e");
+    
+    public static void Main(string[] args)
+    {
+
+        string[] options = new string[]
+        {
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+            "Option 5"
+        };
+        ArrowMenu arrowMenu = new ArrowMenu(LineStyle.THICK);
+        int answer = arrowMenu.Menu(options);
+        Console.WriteLine("Answer: " + (answer + 1));
 
 
-// Stop the program from closing
-Console.ReadKey();
+        Console.ReadKey();
+    }
+}
