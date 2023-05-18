3f8c4a4fe439e4a79043ea80c32386f6d047fc8f
MaximilianMcC <max@maximilian.co.nz>
2023-05-18 11:09:43 +1000

    Updated to use the new settings

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     
     public static void Main(string[] args)
     {
-
+        // Create an array of different options
         string[] options = new string[]
         {
             "Option 1",
@@ -12,8 +12,13 @@
             "Option 4",
             "Option 5"
         };
-        ArrowMenu arrowMenu = new ArrowMenu(LineStyle.THICK);
-        int answer = arrowMenu.Menu(options);
+
+        // Make an arrow menu with settings
+        MenuSettings settings = new MenuSettings(LineStyle.CLASSIC, 10);
+        ArrowMenu arrowMenu = new ArrowMenu(settings);
+
+        // Run the menu and get the answer
+        int answer = arrowMenu.Menu("Test 123", options);
         Console.WriteLine("Answer: " + (answer + 1));
 
 
